5e29ca1113f5a6fd15bedbbbb2dba8ef0ab48b83
conrad mugabe <conradmugabe@gmail.com>
2022-11-28 12:45:33 +0300

    feat: added function and hook for adding file to sotrage bucket

--- src/UI/hooks/useFileSystemService.ts
+++ src/UI/hooks/useFileSystemService.ts
@@ -6,7 +6,7 @@ import {
 } from '@tanstack/react-query';
 import { CONTENTS, FOLDERS } from '../../core/constants';
 import { FileSystemNode } from '../../core/entities/file.system.node';
-import { addFolder, getFolderContents } from '../../web/api/file.system';
+import { addFile, addFolder, getFolderContents } from '../../web/api/file.system';
 import { useUser } from '../context/user-context';
 
 export const useFolderContents = (
@@ -33,6 +33,21 @@ export const useAddFolder = () => {
       const files = queryClient.getQueryData<FileSystemNode[]>(queryKey);
       const updatedFiles = files ? [...files, data] : [data];
       queryClient.setQueryData(queryKey, updatedFiles);
+    },
+  });
+};
+
+export const useAddFile = () => {
+  const queryClient = useQueryClient();
+  const { user } = useUser();
+  return useMutation({
+    mutationFn: addFile,
+    onSuccess(data, { parentFolderId }) {
+      // const id = parentFolderId ? parentFolderId : user?.id || '';
+      // const queryKey = [FOLDERS, CONTENTS, id];
+      // const files = queryClient.getQueryData<FileSystemNode[]>(queryKey);
+      // const updatedFiles = files ? [...files, data] : [data];
+      // queryClient.setQueryData(queryKey, updatedFiles);
     },
   });
 };
--- src/web/api/file.system.ts
+++ src/web/api/file.system.ts
@@ -1,18 +1,21 @@
-import { API_URL } from '../../core/constants';
+import { FILE_SYSTEM_API_URL } from '../../core/constants';
 import { FileSystemNode } from '../../core/entities/file.system.node';
 import { requests } from '../apiClient';
+import { getUploadKey, uploadFile } from './file.upload';
 
 type AddFolderRequest = {
   name: string;
   parentFolderId?: string;
 };
 
+type AddFileRequest = { file: File } & AddFolderRequest;
+
 export const getFolderContents = async (
   folderId?: string
 ): Promise<FileSystemNode[]> => {
   const URL = 'folders/contents';
   const FULL_URL = folderId ? URL + `/${folderId}` : URL;
-  return requests.get(API_URL + FULL_URL);
+  return requests.get(FILE_SYSTEM_API_URL + FULL_URL);
 };
 
 export const addFolder = async ({
@@ -20,5 +23,12 @@ export const addFolder = async ({
   parentFolderId,
 }: AddFolderRequest): Promise<FileSystemNode> => {
   const URL = 'folders';
-  return requests.post(API_URL + URL, { name, parentFolderId });
+  return requests.post(FILE_SYSTEM_API_URL + URL, { name, parentFolderId });
+};
+
+export const addFile = async (data: AddFileRequest) => {
+  const response = await getUploadKey();
+  const res = await uploadFile(response.signedUrl, data.file);
+  console.log('aws response', res);
+  return;
 };
