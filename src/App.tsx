71f5dc1c88c7f59edab77bda904aaacbcd33c39e
conrad mugabe <conradmugabe@gmail.com>
2023-01-10 02:39:33 +0300

    feat: added validation of current user

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { ChakraProvider, Spinner } from '@chakra-ui/react';
 import React from 'react';
 import { useUser } from '@context/user.context';
+import { useAuth } from '@cache/users';
 const AuthenticatedApp = React.lazy(() => import('./AuthenticatedApp'));
 const UnauthenticatedApp = React.lazy(() => import('./UnauthenticatedApp'));
 
 const App = () => {
   const { user } = useUser();
+  const { useCurrentUser } = useAuth();
+  const { isLoading } = useCurrentUser();
+
+  if (isLoading) return <Spinner size="xl" />;
 
   return (
     <ChakraProvider>
