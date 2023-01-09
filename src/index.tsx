80ebd0eee30e76d6f157398e58a25507d3c14417
conrad mugabe <conradmugabe@gmail.com>
2023-01-09 13:21:38 +0300

    feat: added use cases context and moved context out of ui folder

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import AppProvider from '@context/app.context';
+import AppProvider from '@src/context/app.context';
 import App from '@src/App';
 import '@src/index.css';
 
