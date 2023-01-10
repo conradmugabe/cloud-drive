2f879eef53d707d6ed624ff02a54beb550de5c13
conrad mugabe <conradmugabe@gmail.com>
2023-01-10 03:32:14 +0300

    feat: setup firebase firestore

--- src/services/firebase/init.ts
+++ src/services/firebase/init.ts
@@ -1,8 +1,24 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
+import {
+  collection,
+  CollectionReference,
+  DocumentData,
+  getFirestore,
+} from 'firebase/firestore';
 import { firebaseConfig } from '@config/firebase';
+import { FileSystemNode } from '@entities/file.system.node.entity';
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const firestore = getFirestore(app);
 
-export { auth };
+const createCollection = <T = DocumentData>(collectionName: string) => {
+  return collection(firestore, collectionName) as CollectionReference<T>;
+};
+
+const databases = {
+  fileSystem: createCollection<Omit<FileSystemNode, 'id'>>('file-system'),
+};
+
+export { auth, databases };
