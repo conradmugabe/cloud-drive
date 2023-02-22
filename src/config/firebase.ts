776111bab6b13d7d36a9f4626264db6fd17bfdd2
conrad mugabe <conradmugabe@gmail.com>
2023-02-22 09:49:00 +0300

    bug fix: added new config options

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,6 +1,7 @@
 export const firebaseConfig = {
   apiKey: import.meta.env.VITE_REACT_APP_FIREBASE_APIKEY,
   authDomain: import.meta.env.VITE_REACT_APP_FIREBASE_AUTHDOMAIN,
+  databaseURL: import.meta.env.VITE_REACT_APP_FIREBASE_DATABASEURL,
   projectId: import.meta.env.VITE_REACT_APP_FIREBASE_PROJECTID,
   storageBucket: import.meta.env.VITE_REACT_APP_FIREBASE_STORAGEBUCKET,
   messagingSenderId: import.meta.env.VITE_REACT_APP_FIREBASE_MESSAGINGSENDERID,
