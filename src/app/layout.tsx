2ec6876e0cbe40941c544c57e46326a75ee450d7
conrad mugabe <conradmugabe@gmail.com>
2023-09-14 23:27:12 +0300

    feat: added husky and prettier

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,15 @@
-import './globals.css';
-import type { Metadata } from 'next';
-import { Inter, Open_Sans } from 'next/font/google';
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter, Open_Sans } from "next/font/google";
 
-const openSans = Open_Sans({ subsets: ['latin'], variable: '--font-family-open-sans' });
+const openSans = Open_Sans({
+  subsets: ["latin"],
+  variable: "--font-family-open-sans",
+});
 
 export const metadata: Metadata = {
-  title: 'Cloud Drive',
-  description: 'Cloud Drive is an online file storage service for your files.',
+  title: "Cloud Drive",
+  description: "Cloud Drive is an online file storage service for your files.",
 };
 
 export default function RootLayout({
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,18 @@
-import type { Config } from 'tailwindcss';
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: { extend: {
-    fontFamily: {
-      'open-sans': 'var(--font-family-open-sans)',
-    }
-  } },
+  theme: {
+    extend: {
+      fontFamily: {
+        "open-sans": "var(--font-family-open-sans)",
+      },
+    },
+  },
   plugins: [],
 };
 export default config;
